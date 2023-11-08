8997b6d53201178498d15ce5465467dc90c47705
HK-Hikaribot <149997214+HK-Hikaribot@users.noreply.github.com>
2023-11-08 09:15:01 +0200

    Update emojimix.js

--- plugins/emojimix.js
+++ plugins/emojimix.js
@@ -14,7 +14,7 @@ reject(err)
 
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 	
-if (!args[0]) throw ` هذا الامر خاص بدمج اثنين ايموجي  في ايموجي واحد سوف اعطيك مثال لتفهم سايكي سايكي:\n ${usedPrefix + command} 😎+🤑`
+if (!args[0]) throw ` هذا الامر خاص بدمج اثنين ايموجي  في ايموجي واحد سوف اعطيك مثال لتفهم  :\n ${usedPrefix + command} 😎+🤑`
 if (!text.includes('+')) throw  `✳️ مثال \n\n📌   \n*${usedPrefix + command}* 😎+🤑`
 let [emoji, emoji2] = text.split`+`
 let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji)}_${encodeURIComponent(emoji2)}`)
